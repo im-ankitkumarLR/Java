7dc8b3bc48fa5e71fe50adf0d22aba0f0654f45e
aknasa <aknasa12@gmail.com>
2022-02-13 19:57:56 +0530

    Userinput

---
+++ Solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner myobj= new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int a =myobj.nextInt();
+        System.out.println("The Entered number is: " + a);
+        
+        
+        
+        
+        
+        
+    }
+}
